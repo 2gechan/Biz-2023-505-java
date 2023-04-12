ee602c592e11966e9daf3f33f447a42bbec6d687
2gechan <7532602@gmail.com>
2023-04-12 16:31:56 +0900

    숫자데이터

---
+++ hello/src/com/callor/hello/HelloGwangju.java
@@ -0,0 +1,18 @@
+package com.callor.hello;
+
+public class HelloGwangju {
+	
+	/*
+	 * main() method는 프로젝트 전체에서
+	 * 한번만 등장하는 것이 원칙이다
+	 * eclipse로 코드를 작성할때는 현재 보고있는
+	 * 클래스 파일의 main() method를 진입점 method로 본다
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("12345qwer");
+		System.out.println(20 + 30);
+		
+	}
+	
+}
--- hello/src/com/callor/hello/HelloKorea.java
+++ hello/src/com/callor/hello/HelloKorea.java
@@ -1,5 +1,25 @@
 package com.callor.hello;
+/*
+ * java의 클래스 파일 만들기
+ * java의 클래스 파일은 Source Code File 이라고 한다
+ * 클래스명.java라는 이름으로 파일이 생성된다
+ * 여기에서는 HelloKorea.java라는 이름으로 파일이 생성
+ * 
+ * "클래스명은" 가급적 2개 이상의 단어를 조합하여 사용한다
+ * "클래스명은" 첫글자는 반드시 대문자를 사용한다
+ * 이후 부터는 영문대소문자, 숫자, 언더바(_)를 사용할 수 있다
+ * 첫글자를 언더바로 사용하기도 하지만 최근 패턴은 권장하지 않는다
+ */
 
-public class HelloKorea {
-
-}
+public class HelloKorea { // block은 blade(중괄호)로 시작
+	
+	/*
+	 * main method
+	 * main method는 명령문 그룹의 규칙이 정해져 있다
+	 * 진입점, 시작점 method 라고 한다
+	 * java로 만든 코드가 시작되는 곳이다
+	 */
+	public static void main(String[] args) {
+		
+	}
+} // end HelloKorea block, blade는 반드시 쌍을 이루어야 한다
